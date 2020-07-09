a06e7da4f7dfcd0a3e37fc17f871390964eaf73d
daredev0504 <61061540+daredev0504@users.noreply.github.com>
2020-07-09 15:43:15 +0100

    added a contact form

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-gsap.to('.logo',{duration:2,x:300,backgroundColor:'#560563',
-borderRadius:'20%', border:"5px solid green",width:'300px',ease:'bounce'});
+import ScrollReveal from 'scrollreveal';
+
+export default new ScrollReveal();
+
