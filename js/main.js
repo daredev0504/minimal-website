04dfecaf68f047af77db7221cc09108202563a4f
daredev0504 <61061540+daredev0504@users.noreply.github.com>
2020-07-09 17:43:03 +0100

    added js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-import ScrollReveal from 'scrollreveal';
+const inputkey = document.getElementById('inputkey');
+const emailkey = document.getElementById('emailkey');
+const subjectkey = document.getElementById('subjectkey');
+const message = document.getElementById('message');
+const submit = document.getElementById('submit');
 
-export default new ScrollReveal();
 
+
+const addToLocalStorage = () =>{
+    const ikey = inputkey.value;
+    const ekey = emailkey.value;
+    const skey = subjectkey.value;
+    const msg = message.value;
+
+    localStorage.setItem('name',`${ikey}`);
+    localStorage.setItem('email',`${ekey}`);
+    localStorage.setItem('subject',`${skey}`);
+    localStorage.setItem('message',`${msg}`);
+    
+
+}
+
+submit.addEventListener('click',addToLocalStorage);
